accac9171388cca7f54978a2b37e4cd442aea811
jesn1219 <jesn1219@gmail.com>
2020-09-15 11:59:54 +0900

    Inital python-ctags added

---
+++ test.sh
@@ -0,0 +1,1 @@
+echo alias python-ctags="ctags -R --fields=+l --languages=python --python-kinds=-iv -f ./tags . $(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")" >> ~/.bashrc
