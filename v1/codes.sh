faddb6881198ddc0ad5c399c56fa028cd3f469dc
jesnk <jskang@gpu-1.novalocal>
2024-11-29 13:15:03 +0900

    code initialize

---
+++ v1/codes.sh
@@ -0,0 +1,21 @@
+
+alias sb='source ~/.bashrc'
+alias wn='watch -d -n0.5 nvidia-smi'
+alias c0='du -h --max-depth=1'
+alias c0w='watch -n1 -d du -h --max-depth=1'
+
+
+
+
+# GPU DEVICE and HF cache path settings
+'''
+export CUDA_VISIBLE_DEVICES=2
+export HF_DATASETS_CACHE="/data/jskang/.cache/huggingface/hub"
+export TRANSFORMERS_CACHE="/data/jskang/.cache/huggingface/hub"
+export HF_HOME="/data/jskang/.cache/huggingface/hub"
+export PATH=$PATH:/home/jskang/.bin
+'''
+
+
+git config --global user.name jesnk
+git config --global credential.helper store
